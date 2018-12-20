7d3a1b1a18a58e02a71b91eeb1ba3ee08939d903
gy_lyons <gy_lyons@163.com>
2018-12-20 09:38:02 +0800

    Initial Commit

---
+++ AdamProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AdamProject
+//
+//  Created by lyons on 2018/12/20.
+//  Copyright © 2018 lyons. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
